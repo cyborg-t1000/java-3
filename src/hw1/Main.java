0308f0868d5ff5263093faf16990c8ef0fa4e559
Maxim Nekrasov <kgtu@mail.ru>
2021-02-03 10:42:26 -0500

    Hw1. Task 2

--- src/hw1/Main.java
+++ src/hw1/Main.java
@@ -1,6 +1,8 @@
 package hw1;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -10,6 +12,14 @@ public class Main {
         Integer[] arr2 = new Integer[]{1, 2, 3, 4, 5};
         swap(arr2, 1, 2);
         System.out.println(Arrays.toString(arr2));
+
+        Integer[] arr3 = new Integer[]{1,2,3,4,5};
+        List<Integer> list3 = arrayToList(arr3);
+        System.out.println(list3);
+
+        String[] arr4 = new String[]{"1","2","3","4","5a"};
+        List<String> list4 = arrayToList(arr4);
+        System.out.println(list4);
     }
 
     public static void swap(Object[] arr, int i, int j) {
@@ -17,4 +27,9 @@ public class Main {
         arr[i] = arr[j];
         arr[j] = temp;
     }
+
+    public static <T> ArrayList<T> arrayToList(T[] arr){
+        return new ArrayList<>(Arrays.asList(arr));
+    }
+
 }
