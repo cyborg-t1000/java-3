a15a70e49cc1e870f18d230f4d231984f166a8aa
Maxim Nekrasov <kgtu@mail.ru>
2021-02-17 12:50:38 -0500

    Hw5, start / finish / tunnel

--- src/hw5/Car.java
+++ src/hw5/Car.java
@@ -1,5 +1,9 @@
 package hw5;
 
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+
 public class Car implements Runnable {
     private static int CARS_COUNT;
     static {
@@ -8,17 +12,23 @@ public class Car implements Runnable {
     private Race race;
     private int speed;
     private String name;
+    private CountDownLatch ready;
+    private CyclicBarrier start;
+    private CountDownLatch finish;
     public String getName() {
         return name;
     }
     public int getSpeed() {
         return speed;
     }
-    public Car(Race race, int speed) {
+    public Car(Race race, int speed, CountDownLatch ready, CyclicBarrier start, CountDownLatch finish) {
         this.race = race;
         this.speed = speed;
         CARS_COUNT++;
         this.name = "Участник #" + CARS_COUNT;
+        this.ready = ready;
+        this.start = start;
+        this.finish = finish;
     }
     @Override
     public void run() {
@@ -29,8 +39,17 @@ public class Car implements Runnable {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        ready.countDown();
+        try {
+            start.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (BrokenBarrierException e) {
+            e.printStackTrace();
+        }
         for (int i = 0; i < race.getStages().size(); i++) {
             race.getStages().get(i).go(this);
         }
+        finish.countDown();
     }
 }
--- src/hw5/MainClass.java
+++ src/hw5/MainClass.java
@@ -1,23 +1,46 @@
 package hw5;
 
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class MainClass {
         public static final int CARS_COUNT = 4;
         public static void main(String[] args) {
             System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
-            Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+            Race race = new Race(new Road(60), new Tunnel(CARS_COUNT/2), new Road(40));
             Car[] cars = new Car[CARS_COUNT];
-            final CountDownLatch start = new CountDownLatch(CARS_COUNT);
+            final CountDownLatch ready = new CountDownLatch(CARS_COUNT);
+            final CyclicBarrier start = new CyclicBarrier(CARS_COUNT + 1);
+            final CountDownLatch finish = new CountDownLatch(CARS_COUNT);
+            final Lock winner = new ReentrantLock();
             for (int i = 0; i < cars.length; i++) {
-                cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+                cars[i] = new Car(race, 20 + (int) (Math.random() * 10), ready, start, finish);
             }
 
             for (int i = 0; i < cars.length; i++) {
                 new Thread(cars[i]).start();
             }
-            start.await();
+            try {
+                ready.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+            try {
+                start.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+            try {
+                finish.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
         }
 }
